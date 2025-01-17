dfea68eb6e8a60113d4932a1eada4560289d0a6e
mkoterski <matthiaskoterski@gmail.com>
2025-01-17 17:52:59 +0100

    colour removal

--- msc/rpi-bench5.sh
+++ msc/rpi-bench5.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# R A S P I   B E N T O
-# 
-# This script serves up a variety of benchmark tests without any fuss.
-# Version 3.02
-# Author: Matthias Koterski
-# Based on AikonCWD's Raspberry Pi Benchmark Test
-#
-# Backlog:
-# - add more tests and make them more demanding for newer hardware
-# - add more options (e.g. report export)
-# - add more colors and overwork UI
-# - syntax updates
-# - support more devices
-# 
-# 3.02 Updates:
-# - modified initial information displayed
-# - add prompt for internet speed test - doesn't work yet and gets skipped
-# 
-# 3.01 Updates:
-# - updated deprecated sysbench syntax
-#
-
 [ "$(whoami)" == "root" ] || { echo "Must be run as sudo!"; exit 1; }
 
 # Install dependencies
@@ -35,9 +13,9 @@ if [ ! `which speedtest-cli` ]; then
   apt-get install -y speedtest-cli
 fi
 
-# Check for command-line argument to manually run Internet speed test
+# Check for command-line argument to omit Internet speed test
 run_speedtest="y"
-if [ "$1" == "--speedtest" ]; then
+if [ "$1" == "--no-speedtest" ]; then
   run_speedtest="n"
 fi
 
@@ -45,13 +23,11 @@ fi
 # Show simple system information
 clear
 sync
-# model_info=$(cat /proc/device-tree/model | tr -d '\0') # Added tr -d '\0' to remove null bytes
+model_info=$(cat /proc/device-tree/model | tr -d '\0')
 os_info=$(lsb_release -d | cut -f2)
-echo -e "\e[92mRaspi Bento - Version 3.02\e[97m"
-echo -e "$(hostname) | $os_info | $(uname -m)"
-#echo -e "$(hostname) | $os_info | $(uname -m) | $model_info"
-echo "$(date +%Y%m%d-%H:%M)"
-echo -e "\e[97m"
+echo "Raspi Bento - Version 3.02"
+echo "$(hostname) | $os_info | $(uname -m) | $model_info"
+echo "$(date +%Y-%m-%d - %H:%M)"
 
 # Show current hardware
 vcgencmd measure_temp
@@ -61,46 +37,39 @@ vcgencmd get_config int | grep sdram_freq
 vcgencmd get_config int | grep gpu_freq
 printf "sd_clock="
 grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}'
-echo -e "\n\e[93m"
+echo
 
-# Run Internet speed test if specified
+# Run Internet speed test if not omitted
 if [ "$run_speedtest" == "y" ]; then
-  echo -e "Running InternetSpeed test...\e[94m"
+  echo "Running InternetSpeed test..."
   speedtest-cli --simple
-  echo -e "\e[93m"
 else
-  echo -e "Skipping InternetSpeed test...\e[93m"
+  echo "Skipping InternetSpeed test..."
 fi
 
-echo -e "Running CPU test...\e[94m"
+echo "Running CPU test..."
 sysbench cpu --cpu-max-prime=5000 --threads=4 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
-echo -e "\e[93m"
 
-echo -e "Running THREADS test...\e[94m"
+echo "Running THREADS test..."
 sysbench threads --thread-yields=4000 --thread-locks=6 --threads=4 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
-echo -e "\e[93m"
 
-echo -e "Running MEMORY test...\e[94m"
+echo "Running MEMORY test..."
 sysbench memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq --threads=4 run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
-echo -e "\e[93m"
 
-echo -e "Running HDPARM test...\e[94m"
+echo "Running HDPARM test..."
 hdparm -t /dev/mmcblk0 | grep Timing
 vcgencmd measure_temp
-echo -e "\e[93m"
 
-echo -e "Running DD WRITE test...\e[94m"
+echo "Running DD WRITE test..."
 rm -f ~/test.tmp && sync && dd if=/dev/zero of=~/test.tmp bs=1M count=512 conv=fsync 2>&1 | grep -v records
 vcgencmd measure_temp
-echo -e "\e[93m"
 
-echo -e "Running DD READ test...\e[94m"
-echo -e 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
+echo "Running DD READ test..."
+echo 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
 vcgencmd measure_temp
 rm -f ~/test.tmp
-echo -e "\e[0m"
 
-echo -e "\e[91mRaspi Bento is done!🍱\e[0m\n"
+echo "Raspi Bento is done!🍱"
